3167a85d702040670bfb36260aff9573106b82f1
Vincent Gao <vingao@Vincents-MacBook-Pro.local>
2020-03-23 15:50:12 -0400

    Assignment 5

--- assingments/JS/Assignment5.js
+++ assingments/JS/Assignment5.js
@@ -4,34 +4,34 @@ $(`#stock-search`).click(function(){
     function(data){
     console.log(data);
 
+    var date = data["Meta Data"]["3. Last Refreshed"];
 
-    // var stock = data["Meta Data"]["2. Symbol"];
-    // var info = data["Meta Data"]["1. Information"];
-    // var open = data["Time Series (Daily)"][date]["1. open"];
-    // var close = data["Time Series (Daily)"][date]["4. close"];
-    // var high = data["Time Series (Daily)"][date]["2. high"];
-    // var low = data["Time Series (Daily)"][date]["3. low"];
-    // var vol = data["Time Series (Daily)"][date]["5. volume"];
-    // $('#searched_for').append(`Got data for ${stock}`);
-    // var difference = close-open;
+    var stock = data["Meta Data"]["2. Symbol"];
+    var info = data["Meta Data"]["1. Information"];
+    var open = data["Time Series (Daily)"][date]["1. open"];
+    var close = data["Time Series (Daily)"][date]["4. close"];
+    var high = data["Time Series (Daily)"][date]["2. high"];
+    var low = data["Time Series (Daily)"][date]["3. low"];
+    var vol = data["Time Series (Daily)"][date]["5. volume"];
+    var difference = close-open;
   
-    // $(".Stock-Name").append(stock);
-    // $(".info").append(info);
-    // $(".stock-open").append(open);
-    // $(".stock-close").append(close);
-    // $(".difference").append(difference);
-    // $(".date").append(date);
-    // $(".high").append(high);
-    // $(".low").append(low);
-    // $(".vol").append(vol);
+    $(".Stock-Name").append(stock);
+    $(".info").append(info);
+    $(".stock-open").append(open);
+    $(".stock-close").append(close);
+    $(".difference").append(difference);
+    $(".date").append(date);
+    $(".high").append(high);
+    $(".low").append(low);
+    $(".vol").append(vol);
     
-    // if(difference < 0){
-    //   $(".stock").css("background-color", "red");
-    // }
+    if(difference < 0){
+      $(".stock").css("background-color", "red");
+    }
 
-    // else{
-    //   $(".stock").css("background-color", "green");
+    else{
+      $(".stock").css("background-color", "green");
 
-    // }
+    }
   });
 });
