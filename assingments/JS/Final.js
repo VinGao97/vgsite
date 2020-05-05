10f17085e2fc5fbdde8031dc90e2214142c6b8e3
vin.gao <vin.gao@aol.com>
2020-05-05 04:05:56 -0400

    Final Project updates

--- assingments/JS/Final.js
+++ assingments/JS/Final.js
@@ -1,3 +1,8 @@
+var pop;
+var delta = 0;
+var birthRate = 0;
+var deathRate = 0;
+var childRate = 0;
   
 $.getJSON("https://api.census.gov/data/2018/pep/population?get=GEONAME,POP&for=us:*&key=f4541c19a88f3ba95f6e377c2ef455b97e6b82b6", 
   function(data){
@@ -5,31 +10,56 @@ $.getJSON("https://api.census.gov/data/2018/pep/population?get=GEONAME,POP&for=u
 
     var input = $("#population");
 
-    pop = data["1"]["1"];
+    pop = parseInt(data["1"]["1"]);
 
-    input.val(pop);
+    input.html(pop);
   }
 );
 
-var pop;
-var inc;
-var dec;
-
 function buttonInc() {
-  inc = setInterval(function(){
-    document.getElementById('population').value = ++pop;
-  },200);
-  clearInterval(dec);
+  delta = 1;
 }
 
 function buttonDec() {
-  dec = setInterval(function(){
-    document.getElementById('population').value = --pop;
-  },200);
-  clearInterval(inc);
+  delta = -1;
+}
+
+function increaseBirthRate() {
+  delta = delta+1;
+  birthRate=birthRate+1;
+  $("#birthRate").html(birthRate);
+}
+
+function childrenPerWomen() {
+  delta = delta+(birthRate*childRate);
+  childRate=childRate+1;
+  $("#childRate").html(childRate);
+}
+
+function increaseDeathRate() {
+  delta = delta-1;
+  deathRate=deathRate+1;
+  $("#deathRate").html(deathRate);
+}
+
+function naturalDisaster() {
+  delta = delta-50;
+  pop = pop-10000;
+  deathRate=deathRate+50;
+  $("#deathRate").html(deathRate);
 }
 
 function Stop() {
-  clearInterval(dec);
-  clearInterval(inc);
+  clearInterval(loop);
 }
+
+function loop() {
+  pop = pop+delta;
+  console.log('in the loop')
+  $("#population").html(pop);
+}
+
+
+$(document).ready(function() {
+  mainLoop = setInterval(loop,200);
+})
